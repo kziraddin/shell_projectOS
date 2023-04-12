0987593f2f6274d1064c89a5ebd115743eee4061
ZIRADDIN KAZIMLI <93098526+kziraddin@users.noreply.github.com>
2023-04-12 01:05:47 -0400

    Update OSproject1.cpp

--- OSproject1.cpp
+++ OSproject1.cpp
@@ -4,11 +4,13 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <sys/wait.h>
-
 using namespace std;
 
+/* ZIRADDIN KAZIMLI ZIKO */
+//https://github.com/kziraddin/shell_projectOS
+
 //to compile use below format
-/* g++ -std=c++11 your_file.cpp -o your_program */
+/* g++ -std=c++11 file_name.cpp -o you_decide */
 
 vector<string>command_history;
 void display_history(){ 
@@ -195,3 +197,33 @@ int main(){
     }
     return 0;
 }
+
+/*
+The code provided is a simple implementation of a shell in C++. It supports some basic functionalities such as 
+executing external commands, displaying the command history, clearing the command history, clearing the screen, 
+and displaying the process ID. The shell is implemented using a loop that reads input from the user and executes 
+the corresponding commands. The input is split into tokens and checked for built-in commands. 
+If the input is not a built-in command, it is added to the command history.
+
+The execute_commands() function is used to execute external commands. It takes a vector of strings as an argument, 
+which represents the command and its arguments. The function converts the vector to an array of char pointers, 
+which is used as an argument to the execvp() function to execute the command. 
+
+The fork() function is used to create a new process 
+to execute the command, and the waitpid() function is used to wait for the child process to finish. 
+
+The display_history() function is used to display the command history. It iterates over the command_history vector and 
+prints each command.
+
+The clear_history() function is used to clear the command history. It simply calls the clear() function of the vector class 
+to clear the command_history vector.
+
+The clear_screen() function is used to clear the screen. It uses the system() function to execute a system command 
+to clear the screen.
+
+The showpid() function is used to display the process ID. It first gets the process ID using the getpid() function and 
+prints it. It then creates three child processes using the fork() function and adds their PIDs to an array. 
+It waits for the child processes to finish using the waitpid() function and then prints the last three child process IDs.
+
+Overall, this implementation provides a simple example of how a shell can be implemented in C++. 
+However, it is not a complete implementation and lacks many of the advanced features found in modern shells.*/
